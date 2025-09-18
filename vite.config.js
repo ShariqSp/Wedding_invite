9fa8e1d8f8ef2475bda693aa7691993645c3a0c1
shariq <shariq.sp@pinelabs.com>
2025-09-18 15:15:28 +0530

    host update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  // ...other config
+  server: {
+    // Allow external host access
+    allowedHosts: ['wedding.shariqsp.com', 'localhost', '18.208.21.144'],
+    host: true  // also listen on all interfaces
+  }
+};
